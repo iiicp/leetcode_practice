e38fa98212fd9a42a5e8492a8867e9a03dbb76d2
caipeng <iiicp@outlook.com>
2020-12-23 20:33:45 +0800

    add Surrounded regions

---
+++ 0130-Surrounded-Regions/main.cpp
@@ -0,0 +1,26 @@
+/**********************************
+ * File:     main.cpp.c
+ *
+ * Author:   caipeng
+ *
+ * Email:    iiicp@outlook.com
+ *
+ * Date:     2020/12/23
+ ***********************************/
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  /// 被环绕的O，说明值为O就一定在棋盘上
+  /// 这是一个退出的很重要的条件
+  void solve(vector<vector<char>>& board) {
+
+  }
+};
+
+int main(){
+  return 0;
+}
